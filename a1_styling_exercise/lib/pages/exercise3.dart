92162df4be31fb6b1f6c1d208d0386bf7758bf84
William Tan <865428527@qq.com>
2021-07-24 14:36:12 +0800

    feat: Exercise3

--- a1_styling_exercise/lib/pages/exercise3.dart
+++ a1_styling_exercise/lib/pages/exercise3.dart
@@ -12,7 +12,40 @@ class Exercise3 extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: null,
+      body: myExercise3(),
+    );
+  }
+}
+
+class myExercise3 extends StatelessWidget {
+  final List<Widget> circles = List.generate(
+    4,
+    (index) => Positioned(
+      left: index * 70.0,
+      width: 100.0,
+      height: 100.0,
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.grey,
+            width: 5,
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(50)),
+        ),
+      ),
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 310, // 不设置具体宽度如何使内部元素居中
+        child: Stack(
+          children: circles,
+          alignment: Alignment.center,
+        ),
+      ),
     );
   }
 }
