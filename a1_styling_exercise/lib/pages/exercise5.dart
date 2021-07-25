907e7f0efa2e9a8a09f3c722f8600e148a41d9a7
William Tan <865428527@qq.com>
2021-07-25 17:12:32 +0800

    feat: Exercise5

--- a1_styling_exercise/lib/pages/exercise5.dart
+++ a1_styling_exercise/lib/pages/exercise5.dart
@@ -12,7 +12,94 @@ class Exercise5 extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: null,
+      body: myExercise5(),
+    );
+  }
+}
+
+class myExercise5 extends StatelessWidget {
+  final List<Widget> bottomBoxes = List.generate(
+    4,
+    (index) => Container(
+      width: 50,
+      height: 50,
+      color: Colors.blueAccent,
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Flex(
+      direction: Axis.vertical,
+      children: [
+        Expanded(
+          flex: 1,
+          child: Container(
+            color: Colors.green,
+            alignment: Alignment.center,
+            child: Container(
+              width: 80,
+              height: 80,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(40.0),
+                color: Colors.purple,
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 5,
+          child: Container(
+            child: Flex(
+              direction: Axis.horizontal,
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: Container(
+                    child: Flex(
+                      direction: Axis.vertical,
+                      children: [
+                        Expanded(
+                          child: Container(
+                            color: Colors.blue[700],
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            color: Colors.yellow[700],
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            color: Colors.blue[700],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    color: Colors.red,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Container(
+            color: Colors.pink,
+            child: Flex(
+              direction: Axis.horizontal,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: bottomBoxes,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
